dd18c304247f810f10143d66a1eea3ee18846851
ddas4548 <ddas4548@gmail.com>
2025-09-19 15:45:03 +0530

    user model created

---
+++ config/models/user.js
@@ -0,0 +1,23 @@
+import mongoose, { model } from "mongoose";
+import bcrypt from "bcryptjs";  
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: { 
+        type: String,
+        unique: true
+    },
+    password: String
+})
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next();
+   this.password =await bcrypt.hash(this.password, 10);
+   next();
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password,this.password)
+}
+
+export const User = mongoose.model('User', userSchema);
