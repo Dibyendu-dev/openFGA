25b9936ac6d1e628812860c7bb80de4cace4d11b
ddas4548 <ddas4548@gmail.com>
2025-09-19 16:15:57 +0530

    user repo layer created

---
+++ repository/user.repository.js
@@ -0,0 +1,15 @@
+import { User } from "../models/user";
+
+async function create(userData) {
+    const user = new User(userData);
+    await user.save();
+    return user;
+}
+
+async function findByEmail(email) {
+    return User.findOne({email})
+}
+
+module.exports = {
+    create, findByEmail
+}
